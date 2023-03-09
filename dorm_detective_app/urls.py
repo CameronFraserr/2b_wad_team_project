e8bfce1dfb9c25367298f30dbd22876fe95b762e
“Dawud Ibrahim"Dawud <2617404i@student.gla.ac.uk>
2023-03-09 15:55:19 +0000

    urls.py and views.py V1.0

--- dorm_detective_app/urls.py
+++ dorm_detective_app/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
 	path('about_us/', views.about_us, name='about_us'),
 	path('faq/', views.faq, name='faq'),
 	path('contact_us/', views.contact_us, name='contact_us'),
-	path('sign_up/', views.sign_up, name='sign_up'),
-	path('login/', views.login, name='login'),
+	path('sign_up/', views.register, name='sign_up'),
+	path('login/', views.user_login, name='login'),
 	path('login/my_account', views.my_account, name='my_account'),
 	path('login/my_account/my_reviews', views.my_reviews, name='my_reviews'),
-	path('logout/', views.logout, name='logout'),
+	path('logout/', views.user_logout, name='logout'),
 	path('restricted/', views.restricted, name='restricted'),
 ]
