f828a19b8c0709460fd8309b9e917fb89ceb0aff
“Dawud Ibrahim"Dawud <2617404i@student.gla.ac.uk>
2023-03-09 15:25:59 +0000

    urls.py

---
+++ dorm_detective_app/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from dorm_detective_app import views
+
+app_name = 'dorm_detective_app'
+
+urlpatterns = [
+	path('home', views.home, name='home'),
+	path('universities/', views.universities, name='universities'),
+	path('universities/<slug:university_name_slug>/', views.university, name='university'),
+	path('univresities/<slug:university_name_slug>/<slug:accomodation_name_slug>/', views.accommodation, name='accommodation'),
+	path('about_us/', views.about_us, name='about_us'),
+	path('faq/', views.faq, name='faq'),
+	path('contact_us/', views.contact_us, name='contact_us'),
+	path('sign_up/', views.sign_up, name='sign_up'),
+	path('login/', views.login, name='login'),
+	path('login/my_account', views.my_account, name='my_account'),
+	path('login/my_account/my_reviews', views.my_reviews, name='my_reviews'),
+	path('logout/', views.logout, name='logout'),
+	path('restricted/', views.restricted, name='restricted'),
+]
