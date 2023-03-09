10bda42835f90c9932afcb489f4ef0a69ed1559d
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-03-09 00:35:36 +0000

    Edit admin panel to contain University and UserProfile tables

--- dorm_detective_app/admin.py
+++ dorm_detective_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from dorm_detective_app.models import University, UserProfile
 
-# Register your models here.
+
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude', 'longitude')
+
+
+admin.site.register(University, UniversityAdmin)
+admin.site.register(UserProfile)
