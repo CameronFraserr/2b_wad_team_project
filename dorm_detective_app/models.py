11e6784e5e5370c9e6e6f9397761e977075aa80d
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-03-09 00:03:11 +0000

    Add University and UserProfile models

--- dorm_detective_app/models.py
+++ dorm_detective_app/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    NAME_MAX_LENGTH = 128
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    latitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=6,
+        validators=[
+            MaxValueValidator(90.0),
+            MinValueValidator(-90.0),
+        ]
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[
+            MaxValueValidator(180.0),
+            MinValueValidator(-180.0),
+        ]
+    )
+
+    class Meta:
+        verbose_name_plural = 'Universities'
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    current_student = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
