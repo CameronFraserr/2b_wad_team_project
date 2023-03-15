d109bc6d22f3a79ce5445de2a437a8ad04c9dbe4
Maksymilian Jankowski <2619901j@student.gla.ac.uk>
2023-03-15 21:37:27 +0000

    implemented login and logout functionality

--- dorm_detective_app/urls.py
+++ dorm_detective_app/urls.py
@@ -5,4 +5,6 @@ app_name = 'dorm_detective_app'
 
 urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- dorm_detective_app/views.py
+++ dorm_detective_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .forms import UserForm, UserProfileForm
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -37,3 +42,27 @@ def sign_up(request):
         profile_form = UserProfileForm()
 
     return render(request, 'dorm_detective_app/sign_up.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('dorm_detective_app:sign_up'))
+            else:
+                return HttpResponse("Your Dorm Detective account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'dorm_detective_app/login.html', {})
+    
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('dorm_detective_app:login'))
