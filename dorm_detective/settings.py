0d4f3b6bbbc38a322898e3ee5781353e5979017e
Maksymilian Jankowski <2619901j@student.gla.ac.uk>
2023-03-15 21:05:09 +0000

    added some user authentication and register.html

--- dorm_detective/settings.py
+++ dorm_detective/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dorm_detective.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -91,9 +91,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6},
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', 
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
