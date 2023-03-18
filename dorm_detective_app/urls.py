8b698d68d87fcea3694735465d4072d2f15600e1
Cameron Fraser <2666928f@student.gla.ac.uk>
2023-03-18 18:56:47 +0000

    Implemented FAQ page

--- dorm_detective_app/urls.py
+++ dorm_detective_app/urls.py
@@ -22,10 +22,9 @@ urlpatterns = [
 	path('restricted/', views.restricted, name='restricted'),
     path('about/', views.about, name="about"),
     path('accounts/register/', CustomRegistrationView.as_view(), name='django_registration_register'),
-
     path('universities/university_of_glasgow/', views.glasgow, name="glasgow"),
     path('glasgow/finnieston_avenue/', views.finnieston_avenue, name="finnieston_avenue"),
-    path('universities', views.universities, name="universities")
+    path('universities', views.universities, name="universities"),
     # path('sign_up/', views.sign_up, name='sign_up'),
     # path('login/', views.user_login, name='login'),
     # path('logout/', views.user_logout, name='logout'),
