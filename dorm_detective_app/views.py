7739c9bfab0058d7258e0ae6e4e67d4eaba80ae8
Cameron Fraser <2666928f@student.gla.ac.uk>
2023-03-18 19:30:58 +0000

    Restructed sign up form to conform with base.html

--- dorm_detective_app/views.py
+++ dorm_detective_app/views.py
@@ -12,19 +12,23 @@ from dorm_detective_app.models import UserProfile
 from datetime import datetime
 from registration.backends.default.views import RegistrationView
 
+
 class CustomRegistrationView(RegistrationView):
     def get_form_class(self):
         return CustomRegistrationForm
+
 
 def index(request):
     context = {}
     template_name = 'dorm_detective_app/index.html'
     return render(request, template_name, context)
 
+
 def about_us(request):
     context = {}
     template_name = 'dorm_detective_app/about.html'
     return render(request, template_name, context)
+
 
 def contact_us(request):
     context = {}
@@ -71,157 +75,160 @@ def accommodation_name(request):
 
 
 def sign_up(request):
-	# A boolean value for telling the template
-	# whether the registration was successful.
-	# Set to False initially. Code changes value to
-	# True when registration succeeds.
-	registered = False
+    # A boolean value for telling the template
+    # whether the registration was successful.
+    # Set to False initially. Code changes value to
+    # True when registration succeeds.
+    registered = False
 
-	# If it's a HTTP POST, we're interested in processing form data.
-	if request.method == 'POST':
-		# Attempt to grab information from the raw form information.
-		# Note that we make use of both UserForm and UserProfileForm.
-		user_form = UserForm(request.POST)
-		profile_form = UserProfileForm(request.POST)
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+        # Attempt to grab information from the raw form information.
+        # Note that we make use of both UserForm and UserProfileForm.
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
 
-		# If the two forms are valid...
-		if user_form.is_valid() and profile_form.is_valid():
-			# Save the user's form data to the database.
-			user = user_form.save()
+        # If the two forms are valid...
+        if user_form.is_valid() and profile_form.is_valid():
+            # Save the user's form data to the database.
+            user = user_form.save()
 
-			# Now we hash the password with the set_password method.
-			# Once hashed, we can update the user object.
-			user.set_password(user.password)
-			user.save()
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            user.set_password(user.password)
+            user.save()
 
-			# Now sort out the UserProfile instance.
-			# Since we need to set the user attribute ourselves,
-			# we set commit=False. This delays saving the model
-			# until we're ready to avoid integrity problems.
-			profile = profile_form.save(commit=False)
-			profile.user = user
+            # Now sort out the UserProfile instance.
+            # Since we need to set the user attribute ourselves,
+            # we set commit=False. This delays saving the model
+            # until we're ready to avoid integrity problems.
+            profile = profile_form.save(commit=False)
+            profile.user = user
 
-			# Did the user provide a profile picture?
-			# If so, we need to get it from the input form and
-			#put it in the UserProfile model.
-			if 'picture' in request.FILES:
-				profile.picture = request.FILES['picture']
+            # Did the user provide a profile picture?
+            # If so, we need to get it from the input form and
+            # put it in the UserProfile model.
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
 
-			# Now we save the UserProfile model instance.
-			profile.save()
+            # Now we save the UserProfile model instance.
+            profile.save()
 
-			# Update our variable to indicate that the template
-			# registration was successful.
-			registered = True
-		else:
-			# Invalid form or forms - mistakes or something else?
-			# Print problems to the terminal.
-			print(user_form.errors, profile_form.errors)
-	else:
-		# Not a HTTP POST, so we render our form using two ModelForm instances.
-		# These forms will be blank, ready for user input.
-		user_form = UserForm()
-		profile_form = UserProfileForm()
+            # Update our variable to indicate that the template
+            # registration was successful.
+            registered = True
+        else:
+            # Invalid form or forms - mistakes or something else?
+            # Print problems to the terminal.
+            print(user_form.errors, profile_form.errors)
+    else:
+        # Not a HTTP POST, so we render our form using two ModelForm instances.
+        # These forms will be blank, ready for user input.
+        user_form = UserForm()
+        profile_form = UserProfileForm()
 
-	# Render the template depending on the context.
-	return render(request,
-		'dorm_detective/sign_up.html',
-		context = {'user_form': user_form,
-		'profile_form': profile_form,
-		'registered': registered})
+    # Render the template depending on the context.
+    return render(request,
+                  'dorm_detective_app/sign_up.html',
+                  context={'user_form': user_form,
+                           'profile_form': profile_form,
+                           'registered': registered})
 
 
 def user_login(request):
-	# If the request is a HTTP POST, try to pull out the relevant information.
-	if request.method == 'POST':
-		# Gather the username and password provided by the user.
-		# This information is obtained from the login form.
-		# We use request.POST.get('<variable>') as opposed
-		# to request.POST['<variable>'], because the
-		# request.POST.get('<variable>') returns None if the
-		# value does not exist, while request.POST['<variable>']
-		# will raise a KeyError exception.
-		username = request.POST.get('username')
-		password = request.POST.get('password')
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+        # request.POST.get('<variable>') returns None if the
+        # value does not exist, while request.POST['<variable>']
+        # will raise a KeyError exception.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-		# Use Django's machinery to attempt to see if the username/password
-		# combination is valid - a User object is returned if it is.
-		user = authenticate(username=username, password=password)
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
 
-		# If we have a User object, the details are correct.
-		# If None (Python's way of representing the absence of a value), no user
-		# with matching credentials was found.
-		if user:
-			# Is the account active? It could have been disabled.
-			if user.is_active:
-				# If the account is valid and active, we can log the user in.
-				# We'll send the user back to the homepage.
-				login(request, user)
-				return redirect(reverse('dorm_detective:home'))
-			else:
-				# An inactive account was used - no logging in!
-				return HttpResponse("Your Dorm Detective account is disabled.")
-		else:
-			# Bad login details were provided. So we can't log the user in.
-			print(f"Invalid login details: {username}, {password}")
-			return HttpResponse("Invalid login details supplied.")
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return redirect(reverse('dorm_detective:home'))
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your Dorm Detective account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
 
-	# The request is not a HTTP POST, so display the login form.
-	# This scenario would most likely be a HTTP GET.
-	else:
-		# No context variables to pass to the template system, hence the
-		# blank dictionary object...
-		return render(request, 'dorm_detective_app/login.html')
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'dorm_detective_app/login.html')
 
 
 @login_required
 def restricted(request):
-	#return HttpResponse("Since you're logged in, you can see this text!")
-	return render(request, 'dorm_detective_app/restricted.html')
+    # return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'dorm_detective_app/restricted.html')
 
 
 # access the view.
 @login_required
 def user_logout(request):
-	# Since we know the user is logged in, we can now just log them out.
-	logout(request)
-	# Take the user back to the homepage.
-	return redirect(reverse('dorm_detective:home'))
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('dorm_detective:home'))
 
 
 # A helper method
 def get_server_side_cookie(request, cookie, default_val=None):
-	val = request.session.get(cookie)
-	if not val:
-		val = default_val
-	return val
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
 
 # Updated the function definition
 def visitor_cookie_handler(request):
-	visits = int(get_server_side_cookie(request, 'visits', '1'))
-	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
-	# If it's been more than a day since the last visit...
-	if (datetime.now() - last_visit_time).days > 0:
-		visits = visits + 1
-		# Update the last visit cookie now that we have updated the count
-		request.session['last_visit'] = str(datetime.now())
-	else:
-		# Set the last visit cookie
-		request.session['last_visit'] = last_visit_cookie
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
 
-	# Update/set the visits cookie
-	request.session['visits'] = visits
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
 
 def about(request):
     response = render(request, 'dorm_detective_app/about.html')
     return response
 
+
 def glasgow(request):
     response = render(request, 'dorm_detective_app/glasgow.html')
     return response
+
 
 def finnieston_avenue(request):
     response = render(request, 'dorm_detective_app/finnieston_avenue.html')
@@ -278,7 +285,7 @@ def finnieston_avenue(request):
 #             return HttpResponse("Invalid login details supplied.")
 #     else:
 #         return render(request, 'dorm_detective_app/login.html', {})
-    
+
 
 # @login_required
 # def user_logout(request):
