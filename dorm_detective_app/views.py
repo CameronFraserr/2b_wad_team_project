7955b211869452c3ed996cf122e7c25718a7df21
Cameron <cameronfraser011@gmail.com>
2023-03-23 16:19:36 +0000

    Added stats to accomodation pages

--- dorm_detective_app/views.py
+++ dorm_detective_app/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from dorm_detective_app.forms import *
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
-from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
@@ -98,13 +95,28 @@ def accommodation(request, university_slug, accommodation_slug):
         accommodation = Accommodation.objects.get(slug=accommodation_slug)
         university = accommodation.university
         reviews = Review.objects.filter(accommodation=accommodation)
+
+        avg_rating = 0
+        for review in reviews:
+            rating = review.rating
+            avg_rating += rating
+
+        rating_no = reviews.count()
+        avg_rating /= rating_no
+
+
+        # accommodation_weekly_rent =
     except Accommodation.DoesNotExist:
         accommodation = None
         university = None
+        avg_rating = None
+        rating_no = None
     except Review.DoesNotExist:
         reviews = None
+        avg_rating = None
+        rating_no = None
 
-    context = {"universities": universities, "accommodation" : accommodation, "university" : university, "reviews" : reviews}
+    context = {"universities": universities, "accommodation" : accommodation, "university" : university, "reviews" : reviews, "avg_rating" : avg_rating, "rating_no" : rating_no}
     template_name = 'dorm_detective_app/accommodation.html'
     return render(request, template_name, context)
 
@@ -258,74 +270,6 @@ def visitor_cookie_handler(request):
 def about(request):
     response = render(request, 'dorm_detective_app/about.html')
     return response
-
-
-def glasgow(request):
-    response = render(request, 'dorm_detective_app/glasgow.html')
-    return response
-
-
-def finnieston_avenue(request):
-    response = render(request, 'dorm_detective_app/finnieston_avenue.html')
-    return response
-
-# def sign_up(request):
-#     registered = False
-
-#     if request.method == 'POST':
-#         user_form = UserForm(data=request.POST)
-#         profile_form = UserProfileForm(data=request.POST)
-
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user = user_form.save(commit=False)  # don't save the user object yet
-
-#             # Validate the password using Django's built-in password validators
-#             try:
-#                 validate_password(user.password)
-#             except ValidationError as e:
-#                 user_form.add_error('password', e)  # Add the password validation error to user_form
-#             else:
-#                 user.set_password(user.password)
-#                 user.save()
-
-#                 profile = profile_form.save(commit=False)
-#                 profile.user = user
-#                 profile.save()
-
-#                 registered = True
-
-#         else:
-#             print(user_form.errors, profile_form.errors)
-#     else:
-#         user_form = UserForm()
-#         profile_form = UserProfileForm()
-
-#     return render(request, 'dorm_detective_app/sign_up.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
-
-# def user_login(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-
-#         user = authenticate(username=username, password=password)
-
-#         if user:
-#             if user.is_active:
-#                 login(request, user)
-#                 return redirect(reverse('dorm_detective_app:sign_up'))
-#             else:
-#                 return HttpResponse("Your Dorm Detective account is disabled.")
-#         else:
-#             print("Invalid login details: {0}, {1}".format(username, password))
-#             return HttpResponse("Invalid login details supplied.")
-#     else:
-#         return render(request, 'dorm_detective_app/login.html', {})
-
-
-# @login_required
-# def user_logout(request):
-#     logout(request)
-#     return redirect(reverse('dorm_detective_app:login'))
 
 def add_like(request):
     if request.method == "POST":
