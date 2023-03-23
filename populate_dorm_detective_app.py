8ca2e569f33a6b51c9eba5f0c8b64299bd707588
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-03-23 04:03:20 +0000

    Edit population script to populate Users, UserProfiles and Reviews of Accommodations

--- populate_dorm_detective_app.py
+++ populate_dorm_detective_app.py
@@ -1,23 +1,107 @@
+import random
+
 import django
 import os
+from PIL import Image
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dorm_detective.settings')
 
 django.setup()
 
-from dorm_detective_app.models import University, Accommodation, UserProfile
+from dorm_detective_app.models import University, Accommodation, UserProfile, User, Review
 
 
 def populate():
+    img = Image.open('glasgow.jpg')
+    # img.show()
+
+    users = [
+        {'username': "NOTABOT",
+         'password': "123asd1vgfA",
+         'student': True,
+        },
+        {'username': "CHATGPT",
+         'password': "NEW_SKYNET123",
+         'student': True,
+        }
+    ]
+
+    murano_revs = [
+        {'title': "WOW!",
+         'description': "AMAZING PLACE! Will surely come back there next year.",
+         'likes': 4,
+         'rating': 5,
+         },
+    ]
+
+    student_apart_revs = [
+        {'title': "Close to Uni",
+         'description': "Very close to the University, but that's about it.",
+         'likes': 2,
+         'rating': 4,
+         },
+        {'title': "Crap!",
+         'description': "Toilets were very dirty.",
+         'likes': 0,
+         'rating': 1,
+         },
+    ]
+
+    finn_avenue_revs = [
+        {'title': "Scam!",
+         'description': "This place doesn't even exist anymore!? Please take it down, it's only confusing people!",
+         'likes': 1,
+         'rating': 1,
+         },
+    ]
+
     uofg_accommodations = [
+        {'name': 'Student Apartments',
+         'description': """Student Apartments offers self-catered undergraduate student houses and student flats based 
+         in traditional tenement buildings which are typically located in close proximity to the University.""",
+         'latitude': 55.874358,
+         'longitude': -4.287646,
+         'rent_max': 144.83,
+         'rent_min': 115.36,
+         'revs': student_apart_revs,
+         },
+        {'name': 'Murano Street Student Village',
+         'description': """Murano Street Student Village is our largest residence and provides self-catered accommodation 
+         for undergraduate students. The self-contained flats comprise of single study bedrooms, a common living/kitchen area, 
+         bathroom and toilet facilities.""",
+         'latitude': 55.882591,
+         'longitude': -4.276740,
+         'rent_max': 134.47,
+         'rent_min': 134.47,
+         'revs': murano_revs,
+         }
+    ]
+
+    uofst_accommodations = [
         {'name': 'Finnieston Avenue',
-         'description': """Finnieston Avenue is a shared accommodation managed by the University of Glasgow and is located in the West End of Glasgow.
-                    \n\n
-                    It provides 300 shared and en-suite rooms for both Undergraduate and Post-graduate students pursing degrees at the University of Glasgow.""",
+         'description': """Finnieston Avenue is a shared accommodation managed by the University of Strathclyde and is located in the West End of Glasgow.
+                \n\n
+                It provides 300 shared and en-suite rooms for both Undergraduate and Post-graduate students pursing degrees at the University of Strathclyde.""",
          'latitude': 25.213212,
          'longitude': 12.232324,
          'rent_max': 150.23,
          'rent_min': 113.23,
-         }
+         'revs': finn_avenue_revs,
+         },
+    ]
+
+    uofcal_accommodations = [
+        {'name': 'Caledonian Court',
+         'description': """Our ‘village within the city’, Caledonian Court, is the perfect place to live if you’re 
+         looking for that home-from-home atmosphere. Living at Caledonian Court is an ideal way to make friends 
+         with people from all over the world, and our international community includes students from places such as Nigeria, 
+         China, Greece, France, Germany and America.""",
+         'latitude': 55.868409,
+         'longitude': -4.248446,
+         'rent_max': 160.23,
+         'rent_min': 110.23,
+         'revs': []
+         },
     ]
 
     universities = {
@@ -39,6 +123,9 @@ def populate():
                         grants and contracts, with an expenditure of £998.5 million.[1] The university has the
                         sixth-largest endowment of any university in the UK. It is a member of Universitas 21, the
                         Russell Group[7] and the Guild of European Research-Intensive Universities.""",
+            'synopsis': """The University of Glasgow is a public research university in Glasgow, Scotland. Founded by papal bull in 1451
+                        [O.S. 1450],[5] it is the fourth-oldest university in the English-speaking world and one of
+                        Scotland's four ancient universities.""",
             'accommodations': uofg_accommodations,
         },
         'University of Strathclyde': {
@@ -46,21 +133,35 @@ def populate():
             'description': """The University of Strathclyde is a public research university located in
                             Glasgow, Scotland. Founded in 1796 as the Andersonian Institute, it is Glasgow's 
                             second-oldest university.""",
-            'accommodations': [],
+            'synopsis': """The University of Strathclyde is a public research university located in
+                            Glasgow, Scotland. Founded in 1796 as the Andersonian Institute, it is Glasgow's 
+                            second-oldest university.""",
+            'accommodations': uofst_accommodations,
         },
         'Glasgow Caledonian University': {
             'latitude': 55.866883, 'longitude': -4.250399, 'website': 'https://www.gcu.ac.uk/',
             'description': """Glasgow Caledonian University, informally GCU, Caledonian or Caley, is a
                             public university in Glasgow, Scotland. It was formed in 1993 by the merger of The Queen's College,
                             Glasgow and Glasgow Polytechnic.""",
-            'accommodations': [],
+            'synopsis': """Glasgow Caledonian University, informally GCU, Caledonian or Caley, is a
+                            public university in Glasgow, Scotland. It was formed in 1993 by the merger of The Queen's College,
+                            Glasgow and Glasgow Polytechnic.""",
+            'accommodations': uofcal_accommodations,
         },
     }
 
+    user_profs = []
+
+    for user in users:
+        user_profs.append(add_user(user['username'], user['password'], user['student']))
+
     for university, uni_data in universities.items():
-        uni = add_university(university, uni_data['latitude'], uni_data['longitude'],uni_data['description'], uni_data['website'])
+        uni = add_university(university, uni_data['latitude'], uni_data['longitude'], uni_data['description'],
+                             uni_data['website'], uni_data['synopsis'], img)
         for a in uni_data['accommodations']:
-            add_accommodation(uni, a['name'], a['description'], a['latitude'], a['longitude'], a['rent_max'], a['rent_min'])
+            accom = add_accommodation(uni, a['name'], a['description'], a['latitude'], a['longitude'], a['rent_max'], a['rent_min'])
+            for rev in a['revs']:
+                add_review(accom, rev['title'], rev['description'], rev['likes'], rev['rating'], random.choice(user_profs))
 
     for uni in University.objects.all():
         print(f'- {uni}')
@@ -69,12 +170,14 @@ def populate():
         print(f'- {acc}')
 
 
-def add_university(name, latitude, longitude, description, website):
+def add_university(name, latitude, longitude, description, website, synopsis, img):
     uni = University.objects.get_or_create(name=name)[0]
     uni.latitude = latitude
     uni.longitude = longitude
     uni.description = description
     uni.website = website
+    uni.synopsis = synopsis
+    uni.image = img
     uni.save()
 
     return uni
@@ -90,6 +193,26 @@ def add_accommodation(uni, name, description, latitude, longitude, rent_max, ren
     accom.save()
 
     return accom
+
+
+def add_user(username, password, is_student):
+    user = User.objects.get_or_create(username=username, password=password)[0]
+    user.save()
+    user_profile = UserProfile.objects.get_or_create(user=user, current_student=is_student)[0]
+    user_profile.save()
+
+    return user_profile
+
+
+def add_review(accommodation, title, description, likes, rating, user):
+    rev = Review.objects.get_or_create(accommodation=accommodation, user=user)[0]
+    rev.title = title
+    rev.description = description
+    rev.likes = likes
+    rev.rating = rating
+    rev.save()
+
+    return rev
 
 
 if __name__ == '__main__':
